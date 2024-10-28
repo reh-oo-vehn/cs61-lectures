d4d1da4ece796e11099476aba7894e6ac56f6802
Eddie Kohler <ekohler@gmail.com>
2024-10-28 13:59:09 -0400

    Update coherence programs

--- storage1/coherence.cc
+++ storage1/coherence.cc
@@ -1,65 +1,67 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
+#include <cstring>
 #include <unistd.h>
 #include <sys/fcntl.h>
 
-#define TESTFILE "consistency-test.txt"
+#define TESTFILE "test.txt"
+static void usage();
 
-// Overwrite the contents of `TESTFILE` with the string `contents`.
-void overwrite(const char* contents) {
+int main(int argc, char** argv) {
+    bool use_stdio;
+    if (argc < 2 || argc > 3) {
+        usage();
+    } else if (strcmp(argv[1], "-y") == 0) {
+        use_stdio = false;
+    } else if (strcmp(argv[1], "-s") == 0) {
+        use_stdio = true;
+    } else {
+        usage();
+    }
+
+    // Overwrite `test.txt`
     FILE* f = fopen(TESTFILE, "w");
-    fputs(contents, f);
+    fputs("CS 61 sucks\n", f);
     fclose(f);
+
+    // Read first part of `test.txt`
+    int fd = open(TESTFILE, O_RDONLY);
+    assert(fd >= 0);
+
+    char buf[1024];
+    fprintf(stderr, "Reading 6 bytes from `test.txt`...\n");
+    ssize_t nr;
+    if (use_stdio) {
+        f = fdopen(fd, "r");
+        assert(f);
+        nr = fread(buf, 1, 6, f);
+    } else {
+        nr = read(fd, buf, 6);
+    }
+    assert(nr == 6);
+
+    // Give user a chance to play
+    fprintf(stderr, "Now modify `test.txt` and then type `exit`!\n");
+    int x = system("sh");
+    (void) x;
+
+    // Read rest of `test.txt`
+    fprintf(stderr, "Reading more of `test.txt`...\n");
+    ssize_t nr2;
+    if (use_stdio) {
+        nr2 = fread(&buf[nr], 1, sizeof(buf) - nr, f);
+    } else {
+        nr2 = read(fd, &buf[nr], sizeof(buf) - nr);
+    }
+    assert(nr2 >= 0 && size_t(nr + nr2) <= sizeof(buf));
+
+    // Print the result
+    fprintf(stderr, "And the answer is:\n");
+    printf("%.*s\n", int(nr + nr2), buf);
 }
 
-size_t read_using_stdio(char buf[BUFSIZ]) {
-    overwrite("CS 61 is terrible!\n");
-    // Contents of `TESTFILE` is now "CS 61 is terrible!\n"
-
-        // Open `TESTFILE` and read first 9 bytes
-        FILE* f = fopen(TESTFILE, "r");
-        size_t nr = fread(buf, 1, 9, f);
-        assert(nr == 9);
-
-        fflush(f);
-
-    overwrite("CS 61 is awesome!!\n");
-    // Contents of `TESTFILE` is now "CS 61 is awesome!!\n"
-
-        // Read remaining bytes of already-open `TESTFILE`
-        nr += fread(buf + nr, 1, BUFSIZ - nr, f);
-        fclose(f);
-        return nr;
-}
-
-size_t read_using_syscalls(char buf[BUFSIZ]) {
-    overwrite("CS 61 is terrible!\n");
-    // Contents of `TESTFILE` is now "CS 61 is terrible!\n"
-
-        // Open `TESTFILE` and read first 9 bytes
-        int fd = open(TESTFILE, O_RDONLY);
-        ssize_t nr = read(fd, buf, 9);
-        assert(nr == 9);
-
-    overwrite("CS 61 is awesome!!\n");
-    // Contents of `TESTFILE` is now "CS 61 is awesome!!\n"
-
-        // Read remaining bytes of already-open `TESTFILE`
-        nr += read(fd, buf + nr, BUFSIZ - nr);
-        close(fd);
-        assert(nr >= 9);
-        return nr;
-}
-
-int main() {
-    char buf[BUFSIZ];
-
-    printf("Reading using stdio:\n");
-    size_t n = read_using_stdio(buf);
-    fwrite(buf, 1, n, stdout);
-
-    printf("\nReading using system calls:\n");
-    n = read_using_syscalls(buf);
-    fwrite(buf, 1, n, stdout);
+static void usage() {
+    fprintf(stderr, "Usage: ./coherence -y (syscalls) OR ./coherence -s (stdio)\n");
+    exit(1);
 }
